62362ced30cd269f1193ed7f743ae70ee75337e5
Agustín Tolaba <agustin.tolaba.aaa@gmail.com>
2023-11-01 19:03:19 -0300

    probando

--- backend/dist/indexESVIEJO.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-console.log('Probando ts!!');
-//# sourceMappingURL=indexESVIEJO.js.map
--- backend/dist/tipovehiculo/tipovehiculo.entity.js
+++
@@ -1,7 +0,0 @@
-export class Tipo_Vehiculo {
-    constructor(id, descripcionTipoVehiculo) {
-        this.id = id;
-        this.descripcionTipoVehiculo = descripcionTipoVehiculo;
-    }
-}
-//# sourceMappingURL=tipovehiculo.entity.js.map
