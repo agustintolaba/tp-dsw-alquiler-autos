f7ee15757bda2caafb2cd3cbc302f4e85fe1a669
Nino <ninotell@gmail.com>
2024-02-07 22:57:37 -0300

    search booking by ID started

--- client/src/app/home/bookings/page.tsx
+++ client/src/app/home/bookings/page.tsx
@@ -5,17 +5,19 @@ import apiClient from "@/services/api";
 import { getBookings } from "@/services/booking";
 import { verifyAdmin } from "@/services/userType";
 import { Reserva } from "@/types";
+import { NUMERIC_FORMAT } from "@/utils/constants";
 import { alertError } from "@/utils/errorHandling";
-import { Box, Button, Typography } from "@mui/material";
+import { SearchOutlined } from "@mui/icons-material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { Fragment, useEffect, useState } from "react";
 
 const Bookings = () => {
-  const router = useRouter();
   const [isAdmin, setIsAdmin] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [bookings, setBookings] = useState<Reserva[]>([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,11 +35,23 @@ const Bookings = () => {
     fetchData();
   }, []);
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (value.length === 0) {
+      setSearch(value);
+      return;
+    }
+    if (!NUMERIC_FORMAT.test(value)) return;
+    setSearch(value);
+  };
+
+  const handleSearchBooking = () => {};
+
   return (
     <LoadableScreen isLoading={isLoading}>
       <div className="flex flex-col items-center p-8 gap-8">
         <div className="flex flex-row w-full flex-wrap gap-4 items-center justify-center sm:justify-between">
-          <span className="text-4xl font-extralight">
+          <span className="text-4xl font-extralight text-center">
             {isAdmin ? "Administración de reservas" : "Mis reservas"}
           </span>
           {!isAdmin && (
@@ -55,9 +69,29 @@ const Bookings = () => {
         ) : (
           <Box>
             {isAdmin && (
-              <Typography variant="h6" className="mb-4">
-                Alquileres de los próximos 30 días
-              </Typography>
+              <Box className="w-full mb-4 gap-4 flex flex-row flex-wrap-reverse justify-center text-center sm:justify-between">
+                <Typography variant="h6">
+                  Alquileres de los próximos 30 días
+                </Typography>
+
+                <Box className="flex flex-row gap-2 items-center">
+                  <TextField
+                    className=""
+                    label="Búsqueda por ID"
+                    placeholder="Ingrese ID de reserva"
+                    variant="outlined"
+                    value={search}
+                    onChange={handleInputChange}
+                  />
+                  <SearchOutlined
+                    className={`${
+                      search.length === 0
+                        ? "text-gray-400 bg-gray-500"
+                        : "text-white bg-slate-600 cursor-pointer hover:bg-slate-400"
+                    } p-1 h-full w-10 rounded transition-all`}
+                  />
+                </Box>
+              </Box>
             )}
             <Box className="flex flex-wrap gap-4">
               {bookings.map((booking) => (
