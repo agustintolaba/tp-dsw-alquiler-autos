5046abba9d1687525e076b5c8ac6bb81ee7e12b4
Agustín Tolaba <agustin.tolaba.aaa@gmail.com>
2024-02-18 12:15:36 -0300

    nueva provincia

--- client/cypress/e2e/newProvincia.spec.cy.ts
+++ client/cypress/e2e/newProvincia.spec.cy.ts
@@ -1,7 +1,20 @@
-describe('template spec', () => {
-  beforeEach()
-  
-  it('passes', () => {
-    cy.visit('https://local')
+describe('Agregar una nueva provincia', () => {
+  beforeEach(()=>{
+    cy.visit('http://localhost:3001')
+    cy.get('[id="simple-tab-0"]').click()
+    cy.get('[type="email"]').type('agus@gmail.com')
+    cy.get('[type="password"]').type('Hola1')
+    cy.get('[type="submit"]').should('not.be.disabled').click()
+    cy.url().should('eq', 'http://localhost:3001/home')
   })
+
+  it('Agregar una nueva provincia satisfactoriamente', () => {
+    cy.get('[data-testid="MenuIcon"]').click()
+    cy.get('[role="button"]').contains('Administrar provincias').click()
+    cy.url().should('eq', 'http://localhost:3001/home/provincia')
+    cy.get('[id=":r0:-label"]').type('ProvinciaTest')
+    cy.get('[type="submit"]').contains('Añadir').click()
+    /*faltaria agregar la validacion de agregada correctamente, despues de sacar el alert*/
+  })
+
 })
