d6c5f2b8dbb199f2a2dcf1a46f4845f74826062b
Agustín Tolaba <agustin.tolaba.aaa@gmail.com>
2024-02-21 14:13:02 -0300

    api

--- client/cypress/e2e/apiRequest.cy.ts
+++ client/cypress/e2e/apiRequest.cy.ts
@@ -1,38 +1,74 @@
 import { Provincia } from "@/types.js"
 const endPoint = 'http://localhost:3000/api/provincia'
 
+let idProv: number = 10
+
 const newProvincia: Provincia = {
-  "id": 0,
-  "descripcion": "ProvinciaTest",
+  "id": idProv,
+  "descripcion": "ProvinciaTest2",
   "localidades": []
 }
 
 const patchProvincia: Provincia ={
-  "id": 0,
-  "descripcion": "ProvinciaTestPatch",
+  "id": idProv,
+  "descripcion": "ProvinciaTestPatch2",
   "localidades": []
 }
+
+const userLogin= { 
+  "email": Cypress.env("userTest"),
+  "password": Cypress.env("passwordTest")
+}
+
+let authToken: string
 
 const addProvincia = (newProvincia: Provincia) =>{
 cy.request({
     method: 'POST',
     url: endPoint,
     headers: {
-      Authorization: 'Bearer yourAuthTokenHere' // Reemplaza con tu token???
+      Authorization: authToken
     },
     body: newProvincia
   })
+  .then((data) => {
+      authToken = data.id;
+    })
 }
 
 const updateProvincia = (patchProvincia: Provincia) =>{
-cy.request('PATCH', `${endPoint}/${patchProvincia.id}`, patchProvincia) //agregat token tambien
-}
+cy.request(
+  {
+    method: 'PATCH',
+    url: `${endPoint}/${patchProvincia.id}`,
+    headers: {
+      Authorization: authToken
+    },
+    body: patchProvincia
+  })}
+
 
 const deleteProvincia = (deleteProvincia: Provincia) =>{
-  cy.request('DELETE', `${endPoint}/${deleteProvincia.id}`) //agregat token tambien
+  cy.request(
+    {
+    method: 'DELETE',
+    url: `${endPoint}/${deleteProvincia.id}`,
+    headers: {
+      Authorization: authToken
+    },
+  })
 }
 
 describe('API testing', ()=>{
+  beforeEach(() => {
+    cy.request('POST', 'http://localhost:3000/api/usuario/login', userLogin)
+    .its('body')
+    .then((body) => {
+      authToken = body.token;
+    });
+  });
+
+
   it('Add a new provincia', ()=>{
     addProvincia(newProvincia)
 
