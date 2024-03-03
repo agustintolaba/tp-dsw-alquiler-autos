68b4c805fd267e8661c48697bba088b93a17c2a7
Agustín Tolaba <agustin.tolaba.aaa@gmail.com>
2024-03-03 17:24:58 -0300

    validacion de rice condition

--- client/src/app/home/bookings/create/page.tsx
+++ client/src/app/home/bookings/create/page.tsx
@@ -1,41 +1,42 @@
-"use client";
-import LoadableScreen from "@/components/LoadableScreen";
-import { Sucursal, Vehiculo } from "@/types";
-import "dayjs/locale/en-gb";
-import { useEffect, useState } from "react";
+'use client';
+import LoadableScreen from '@/components/LoadableScreen';
+import { Sucursal, Vehiculo } from '@/types';
+import 'dayjs/locale/en-gb';
+import { useEffect, useState } from 'react';
 import CreateBookingForm, {
   CreateBookingFormData,
-} from "@/components/forms/CreateBookingForm";
-import useAdmin from "@/services/userType";
-import VehicleListItem from "@/components/items/VehicleItem";
-import { Button, Typography } from "@mui/material";
-import dayjs, { Dayjs } from "dayjs";
+} from '@/components/forms/CreateBookingForm';
+import useAdmin from '@/services/userType';
+import VehicleListItem from '@/components/items/VehicleItem';
+import { Button, Typography } from '@mui/material';
+import dayjs, { Dayjs } from 'dayjs';
 import {
   MAX_WORKING_HOUR,
   NINE_AM,
   transmisionDescriptions,
-} from "@/utils/constants";
-import Image from "next/image";
-import apiClient from "@/services/api";
+} from '@/utils/constants';
+import Image from 'next/image';
+import apiClient from '@/services/api';
 import ConfirmModal, {
   ConfirmModalData,
-} from "@/components/modals/ConfirmationModal";
+} from '@/components/modals/ConfirmationModal';
 
 const getBookingDateDefaultValue = (isDateFrom: boolean): Dayjs => {
   const isLateForToday = dayjs().hour() > MAX_WORKING_HOUR;
   if (isLateForToday)
-    return isDateFrom ? NINE_AM.add(1, "day") : NINE_AM.add(2, "day");
+    return isDateFrom ? NINE_AM.add(1, 'day') : NINE_AM.add(2, 'day');
 
-  return isDateFrom ? dayjs().add(1, "day") : dayjs().add(2, "day");
+  return isDateFrom ? dayjs().add(1, 'day') : dayjs().add(2, 'day');
 };
 
 const CreateBooking = () => {
   const { isAdmin, isLoadingAdmin } = useAdmin();
   const [confirmModal, setConfirmModal] = useState<ConfirmModalData>();
+  const [openModal, setOpenModal] = useState<boolean>(false);
   const [formData, setFormData] = useState<CreateBookingFormData>({
     fechaDesde: getBookingDateDefaultValue(true),
     fechaHasta: getBookingDateDefaultValue(false),
-    transmision: "AT",
+    transmision: 'AT',
     tipoVehiculo: 1,
     sucursal: 1,
   });
@@ -51,7 +52,7 @@ const CreateBooking = () => {
       .then((res) => {
         const vehiculo: Vehiculo = res.data.vehicle;
         const sucursal = vehiculo.sucursal;
-        window.scrollTo({ top: 0, behavior: "smooth" });
+        window.scrollTo({ top: 0, behavior: 'smooth' });
         setConfirmModal({
           formData,
           vehicle: vehiculo,
@@ -62,12 +63,14 @@ const CreateBooking = () => {
 
   return (
     <LoadableScreen isLoading={isLoadingAdmin}>
-      <div className={`${confirmModal ? "h-screen overflow-hidden" : ""}`}>
+      <div className={`${confirmModal ? 'h-screen overflow-hidden' : ''}`}>
         {confirmModal ? (
           <ConfirmModal
             open={confirmModal !== undefined}
             confirmData={confirmModal}
-            cancel={() => setConfirmModal(undefined)}
+            cancel={() => {
+              setConfirmModal(undefined);
+            }}
           />
         ) : (
           <></>
