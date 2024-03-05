b7ea073a5ec9541455c4dc290453efa720f280b7
Agustín Tolaba <agustin.tolaba.aaa@gmail.com>
2024-03-05 19:29:23 -0300

    archivos para deploy

--- backend/src/shared/db/orm.ts
+++ backend/src/shared/db/orm.ts
@@ -1,25 +1,26 @@
-import { MikroORM } from "@mikro-orm/core";
-import { SqlHighlighter } from "@mikro-orm/sql-highlighter";
-import dotenv from "dotenv";
+import { MikroORM } from '@mikro-orm/core';
+import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
+import dotenv from 'dotenv';
 dotenv.config();
 
 export const orm = await MikroORM.init({
   entities: ['dist/**/*.entity.js'],
-  entitiesTs : ['src/**/*.entity.ts'],
+  entitiesTs: ['src/**/*.entity.ts'],
   dbName: 'agenciadsw',
   type: 'mysql',
-  clientUrl: `mysql://root:${process.env.DB_PASSWORD}@localhost:3306/agenciadsw`,
+  clientUrl: `mysql://root:${process.env.DB_PASSWORD}@mysqldb:${process.env.MYSQL_LOCAL_PORT}/agenciadsw`, //modificar la conexion con .env
   highlighter: new SqlHighlighter(),
   debug: true,
-  schemaGenerator: { //NUNCA USAR EN PRODUCCION, PUEDE BORRAR LA BASE DE DATOS
+  schemaGenerator: {
+    //NUNCA USAR EN PRODUCCION, PUEDE BORRAR LA BASE DE DATOS
     disableForeignKeys: true,
     createForeignKeyConstraints: true,
     ignoreSchema: [],
   },
-}) 
+});
 
-export const syncSchema = async ()=> {
-  const generator = orm.getSchemaGenerator()
+export const syncSchema = async () => {
+  const generator = orm.getSchemaGenerator();
   /*puede ir antes un drop y create*/
-  await generator.updateSchema()
-}
+  await generator.updateSchema();
+};
